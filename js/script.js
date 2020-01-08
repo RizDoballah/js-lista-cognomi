a2f3f699e94f387cbdc25127482fb8f1fb2a04f3
Ghezlene Doballah <ghyzounette@gmail.com>
2020-01-08 16:50:56 +0100

    array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-alert('hello');
+var cognomeUser = prompt('inserisci il tuo cognome');
+console.log(cognomeUser);
+var cognomi = ['merceds', 'renault', 'kia', 'rangerover', 'fiat'];
+cognomi.sort();
+console.log(cognomi);
+document.getElementById("lista").innerHTML = cognomi;
+
+
+
+
+
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi,
+// stampa la lista ordinata alfabeticamente (in html ul > li).
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova.
